002c2396a40b40803ee9f254eee8a0a5385d8b1a
PravarChaurasia <praverchaurasia@gmail.com>
2019-10-20 10:36:23 +0530

    Add files via upload
    https://practice.geeksforgeeks.org/problems/anagram/0

---
+++ String/Anagram.java
@@ -0,0 +1,175 @@
+// Created by Pravar
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class Anagram 
+{ 
+  static class Reader 
+    { 
+        final private int BUFFER_SIZE = 1 << 16; 
+        private DataInputStream din; 
+        private byte[] buffer; 
+        private int bufferPointer, bytesRead; 
+
+        public Reader() 
+        { 
+            din = new DataInputStream(System.in); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+
+        public Reader(String file_name) throws IOException 
+        { 
+            din = new DataInputStream(new FileInputStream(file_name)); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+
+        public String readLine() throws IOException 
+        { 
+            byte[] buf = new byte[64]; // line length 
+            int cnt = 0, c; 
+            while ((c = read()) != -1) 
+            { 
+                if (c == '\n') 
+                    break; 
+                buf[cnt++] = (byte) c; 
+            } 
+            return new String(buf, 0, cnt); 
+        } 
+
+        public int nextInt() throws IOException 
+        { 
+            int ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do
+            { 
+                ret = ret * 10 + c - '0'; 
+            } while ((c = read()) >= '0' && c <= '9'); 
+
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+
+        public long nextLong() throws IOException 
+        { 
+            long ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+
+        public double nextDouble() throws IOException 
+        { 
+            double ret = 0, div = 1; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+
+            if (c == '.') 
+            { 
+                while ((c = read()) >= '0' && c <= '9') 
+                { 
+                    ret += (c - '0') / (div *= 10); 
+                } 
+            } 
+
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+
+        private void fillBuffer() throws IOException 
+        { 
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+            if (bytesRead == -1) 
+                buffer[0] = -1; 
+        } 
+
+        private byte read() throws IOException 
+        { 
+            if (bufferPointer == bytesRead) 
+                fillBuffer(); 
+            return buffer[bufferPointer++]; 
+        } 
+
+        public void close() throws IOException 
+        { 
+            if (din == null) 
+                return; 
+            din.close(); 
+        } 
+    } 
+    public static void main(String[] args) throws IOException 
+    { 
+        // BufferedReader Class for Fast buffer Input 
+        BufferedReader br1 = new BufferedReader( 
+                               new FileReader("input.txt")); 
+  
+        // PrintWriter class prints formatted representations 
+        // of objects to a text-output stream. 
+        PrintWriter pw=new PrintWriter(new
+                BufferedWriter(new FileWriter("output.txt"))); 
+  
+        // Your code goes Here 
+        Reader ip=new Reader();
+        int test=ip.nextInt();
+        while(test!=0)
+        {
+            //code
+            String s1=ip.readLine();
+            String s2=ip.readLine();
+
+            int sizeEqual=0,sum=0;    // flags
+            int sumsc1=0,sumsc2=0;
+
+            char sc1[]=s1.toCharArray();
+            char sc2[]=s2.toCharArray();
+
+            if(sc1.length==sc2.length)
+            	sizeEqual=1;
+            
+            for(int i=0;i<sc1.length;i++)
+            	sumsc1+=sc1[i]-'a';
+            for(int i=0;i<sc2.length;i++)
+            	sumsc2+=sc2[i]-'a';
+            if(sumsc1==sumsc2)
+            	sum=1;
+            if(sizeEqual==1 && sum==1)
+            	System.out.println("YES");
+            else
+            	System.out.println("NO");
+
+            test--;
+        }
+
+  
+        pw.flush();    // clear the buffer
+    } 
+}
